84a5f17584cba2c46197af67abb444d1af9e392f
Tobias <tobias@cynkra.com>
2019-12-17 16:22:25 +0100

    tryCatch()

--- plumber.R
+++ plumber.R
@@ -8,7 +8,7 @@ NULL
 #' @serializer contentType list(type="application/json")
 #' @response 200 OK
 #' @get /all
-all <- function() {
+all <- function(res) {
   paste(readLines("docs/all.json"), collapse = "\n")
 }
 
@@ -19,7 +19,14 @@ all <- function() {
 #' @serializer contentType list(type="application/json")
 #' @response 200 OK
 #' @get /by-package
-by_package <- function(name) {
+by_package <- function(res, name) {
   path <- file.path("docs/by-package", paste0(name, ".json"))
-  paste(readLines(path), collapse = "\n")
+  tryCatch(
+    paste(readLines(path), collapse = "\n"),
+    error = function(e) {
+      res$status <- 500
+      # res$status = 400  # the response object that is always available in plumber functions
+      jsonlite::toJSON(list(error = conditionMessage(e))) #, traceback = ...))
+      })
 }
+
