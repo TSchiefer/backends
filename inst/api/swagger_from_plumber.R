dc28097a888316e7b7977d69ad3b8b2fb1a550c7
Tobias <tobias@cynkra.com>
2019-12-18 13:54:04 +0100

    upd paths

--- inst/api/swagger_from_plumber.R
+++ inst/api/swagger_from_plumber.R
@@ -1,6 +1,6 @@
 library(magrittr)
 
-plumber <- plumber::plumb("plumber.R")
+plumber <- plumber::plumb("inst/api/plumber.R")
 plumber$run(swagger = TRUE)
 
 # openapi.json comes from saving the swagger-file that is accessible
@@ -8,8 +8,8 @@ plumber$run(swagger = TRUE)
 # `plumber$run(port = 8961, swagger = TRUE)`
 
 # converting json to yaml:
-jsonlite::read_json("openapi.json") %>%
-  yaml::write_yaml("openapi.yaml")
+jsonlite::read_json("inst/api/openapi.json") %>%
+  yaml::write_yaml("inst/api/openapi.yaml")
 
 # by default the produced swagger/openapi-file is (currently) one
 # of version 3.0.2 if an earlier version is required, a manual
@@ -19,8 +19,8 @@ jsonlite::read_json("openapi.json") %>%
 # specifications of the endpoints is still given -- then used
 # as the swagger-file to host the API.
 
-plumber <- plumber::plumb("plumber.R")
+plumber <- plumber::plumb("inst/api/plumber.R")
 plumber$run(swagger = function(pr, spec, ...) {
-  spec <- yaml::read_yaml("openapi.yaml")
+  spec <- yaml::read_yaml("inst/api/openapi.yaml")
   spec
 })
