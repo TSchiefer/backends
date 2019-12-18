0d405358267ae377135c3b821c681f3212d01ad4
Tobias <tobias@cynkra.com>
2019-12-18 09:01:15 +0100

    refactor plumber.R

--- plumber.R
+++ plumber.R
@@ -20,23 +20,21 @@ all <- function(res) {
 #' @response 200 OK
 #' @get /by-package
 by_package <- function(res, name) {
-  tryCatch(
-    path <- file.path("docs/by-package", paste0(name, ".json")),
-    error = function(e) {
-      res$status <- 400
-    })
-  if (res$status != 400) {
-    tryCatch(
-      paste(readLines(path), collapse = "\n"),
-      error = function(e) {
-        res$status <- 500
-        jsonlite::toJSON(
-          list(
-            error = paste0("Please verify package name: ", name)))
-      })
-    } else jsonlite::toJSON(list(
-        error =
-          "Your request did not include the package name (required).")
-      )
+  if (rlang::is_missing(name)) {
+    res$status <- 400
+    jsonlite::toJSON(list(
+      error =
+        "Your request did not include the package name (required).")
+    )} else {
+      path <- file.path("docs/by-package", paste0(name, ".json"))
+      tryCatch(
+        paste(readLines(path), collapse = "\n"),
+        error = function(e) {
+          res$status <- 500
+          jsonlite::toJSON(
+            list(
+              error = paste0("Please verify package name: ", name)))
+        })
+    }
 }
 
